426e17336ee8d83e0708f7f3bd9a6ec1ea304fd7
nathanjent <nathanjent@gmail.com>
2016-03-29 06:36:28 -0400

    Update to build

--- src/edu/wright/cs4840/mixturemining/MMApp.java
+++ src/edu/wright/cs4840/mixturemining/MMApp.java
@@ -1,14 +1,12 @@
 package edu.wright.cs4840.mixturemining;
 
 import java.util.Enumeration;
-
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-
 import weka.attributeSelection.BestFirst;
 import weka.attributeSelection.CfsSubsetEval;
 import weka.classifiers.Classifier;
@@ -25,9 +23,9 @@ public class MMApp {
 	public static final long serialVersionUID = 1L;
 	private static Instances trainingData;
 	private static Instances testData;
-  
+
 	public static void main(String[] args) {
-		
+
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = null;
 		try {
@@ -48,7 +46,7 @@ public class MMApp {
 		        System.err.println("File load failed. Reason: " + exp.getMessage());
 			}
 		}
-		
+
 		// Set class attribute from first row in file.
 		if (trainingData.classIndex() == -1) {
 			trainingData.setClassIndex(0);
@@ -56,7 +54,7 @@ public class MMApp {
 		if (testData.classIndex() == -1) {
 			testData.setClassIndex(0);
 		}
-		
+
 		if (cmd.hasOption(Ops.FILTER.getName())) {
 			AttributeSelection attrFilter = new AttributeSelection();
 			CfsSubsetEval attrEvaluator = new CfsSubsetEval();
@@ -67,30 +65,31 @@ public class MMApp {
 			try {
 				// set filter for selection from training data
 				attrFilter.setInputFormat(trainingData);
-				
+
 				// apply filters to data
 				Instances reduced_data_training = Filter.useFilter(trainingData, attrFilter);
 				Instances reduced_data_testing = Filter.useFilter(testData, attrFilter);
-				
+
 				// should print out selected features
 				Enumeration<Attribute> attributes_training = reduced_data_training.enumerateAttributes();
 				while (attributes_training.hasMoreElements()) {
-					Attribute attr = (Attribute) attributes_training.nextElement();
+					Attribute attr = attributes_training.nextElement();
 					System.out.println(reduced_data_training.attributeStats(attr.index()));
 				}
 				Enumeration<Attribute> attributes_testing = reduced_data_testing.enumerateAttributes();
 				while (attributes_testing.hasMoreElements()) {
-					Attribute attr = (Attribute) attributes_testing.nextElement();
+					Attribute attr = attributes_testing.nextElement();
 					System.out.println(reduced_data_testing.attributeStats(attr.index()));
 				}
-				
-				
+
 				// apply classifier
-				Classifier classifier = new NaiveBayes();
+				NaiveBayes classifier = new NaiveBayes();
 				classifier.buildClassifier(reduced_data_training);
+
+				// should print out classifier data
+				System.out.println(classifier);
+
 				Evaluation evaluator = new Evaluation(reduced_data_testing);
-				
-				//			 should print out classifier data 
 
 				// should print out evaluator data
 				System.out.println(evaluator.rootMeanPriorSquaredError());
@@ -107,9 +106,9 @@ public class MMApp {
 		FILTER(Option.builder("-f").hasArg()
 				.desc("filter setting: bestfirst, bestlast")
 				.longOpt("filter").build());
-		
+
 		private Option op;
-		
+
 		Ops(Option op) {
 			this.op = op;
 		}
