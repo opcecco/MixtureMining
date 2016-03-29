bdefd27531a916b27540c044d073069adc17d9cb
nathanjent <nathanjent@gmail.com>
2016-03-28 22:52:33 -0400

    Working on filter.

--- src/edu/wright/cs4840/mixturemining/MMApp.java
+++ src/edu/wright/cs4840/mixturemining/MMApp.java
@@ -1,5 +1,7 @@
 package edu.wright.cs4840.mixturemining;
 
+import java.util.Enumeration;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -9,6 +11,10 @@ import org.apache.commons.cli.ParseException;
 
 import weka.attributeSelection.BestFirst;
 import weka.attributeSelection.CfsSubsetEval;
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.Evaluation;
+import weka.core.Attribute;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
@@ -29,10 +35,11 @@ public class MMApp {
 		} catch (ParseException exp) {
 	        System.err.println("Parsing failed. Reason: " + exp.getMessage());
 		}
-		
+		System.out.println("Command: " + cmd);
 		if (cmd.hasOption(Ops.DATA.getName())) {
 			String trainFileName = Ops.DATA.op.getValue(1);
 			String testFileName = Ops.DATA.op.getValue(2);
+			System.out.println("Loading files: " + trainFileName + ' ' + testFileName);
 			try {
 				// Read accepts cvs, arff, or xrff file extensions
 				trainingData = DataSource.read(trainFileName);
@@ -52,19 +59,41 @@ public class MMApp {
 		
 		if (cmd.hasOption(Ops.FILTER.getName())) {
 			AttributeSelection attrFilter = new AttributeSelection();
-			CfsSubsetEval evaluator = new CfsSubsetEval();
+			CfsSubsetEval attrEvaluator = new CfsSubsetEval();
 			BestFirst search = new BestFirst();
 
-			attrFilter.setEvaluator(evaluator);
+			attrFilter.setEvaluator(attrEvaluator);
 			attrFilter.setSearch(search);
 			try {
 				// set filter for selection from training data
 				attrFilter.setInputFormat(trainingData);
 				
 				// apply filters to data
-				
 				Instances reduced_data_training = Filter.useFilter(trainingData, attrFilter);
 				Instances reduced_data_testing = Filter.useFilter(testData, attrFilter);
+				
+				// should print out selected features
+				Enumeration<Attribute> attributes_training = reduced_data_training.enumerateAttributes();
+				while (attributes_training.hasMoreElements()) {
+					Attribute attr = (Attribute) attributes_training.nextElement();
+					System.out.println(reduced_data_training.attributeStats(attr.index()));
+				}
+				Enumeration<Attribute> attributes_testing = reduced_data_testing.enumerateAttributes();
+				while (attributes_testing.hasMoreElements()) {
+					Attribute attr = (Attribute) attributes_testing.nextElement();
+					System.out.println(reduced_data_testing.attributeStats(attr.index()));
+				}
+				
+				
+				// apply classifier
+				Classifier classifier = new NaiveBayes();
+				classifier.buildClassifier(reduced_data_training);
+				Evaluation evaluator = new Evaluation(reduced_data_testing);
+				
+				//			 should print out classifier data 
+
+				// should print out evaluator data
+				System.out.println(evaluator.rootMeanPriorSquaredError());
 			} catch (Exception exp) {
 		        System.err.println("Attribute selection filter failed. Reason: " + exp.getMessage());
 			}
@@ -72,10 +101,10 @@ public class MMApp {
 	}
 
 	private enum Ops {
-		DATA(Option.builder("d").argName( "trainingfile testfile" ).hasArg()
+		DATA(Option.builder("-d").argName("trainingfile testfile" ).hasArg()
 				.desc("load training and test data files").numberOfArgs(2)
 				.longOpt("loaddata").build()),
-		FILTER(Option.builder("f").hasArg()
+		FILTER(Option.builder("-f").hasArg()
 				.desc("filter setting: bestfirst, bestlast")
 				.longOpt("filter").build());
 		
