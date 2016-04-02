9e2a338e4e4ce5f6054cdb1647dd02c8bedd7066
Nathan Adams <adams.201@wright.edu>
2016-04-01 20:26:41 -0400

    Updated MMApp.java for use with current training/test file format.

--- src/edu/wright/cs4840/mixturemining/MMApp.java
+++ src/edu/wright/cs4840/mixturemining/MMApp.java
@@ -1,9 +1,10 @@
 package edu.wright.cs4840.mixturemining;
 
+import java.io.File;
 import java.util.Enumeration;
-import weka.attributeSelection.BestFirst;
+
 import weka.attributeSelection.CfsSubsetEval;
-import weka.classifiers.Classifier;
+import weka.attributeSelection.GreedyStepwise;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.evaluation.Evaluation;
 import weka.core.Attribute;
@@ -11,8 +12,6 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
-import java.io.IOException;
-import java.io.File;
 
 public class MMApp {
 
@@ -28,6 +27,7 @@ public class MMApp {
 
 		File trainFile = new File(args[0]);
 		File testFile = new File(args[1]);
+		int numToSelect = Integer.parseInt(args[2]);
 		
 		System.out.println("Loading training file: " + trainFile.getPath());
 		System.out.println("Loading testing file: " + testFile.getPath());
@@ -43,16 +43,22 @@ System.out.println("Working Directory = " + System.getProperty("user.dir"));
 
 			// Set class attribute from first row in file.
 			if (trainingData.classIndex() == -1) {
-				trainingData.setClassIndex(0);
+				trainingData.setClassIndex(1);
 			}
 			if (testData.classIndex() == -1) {
-				testData.setClassIndex(0);
+				testData.setClassIndex(1);
 			}
 
 			AttributeSelection attrFilter = new AttributeSelection();
 			CfsSubsetEval attrEvaluator = new CfsSubsetEval();
-			BestFirst search = new BestFirst();
-
+			
+			GreedyStepwise search = new GreedyStepwise();
+			
+			search.setNumToSelect(numToSelect);
+			search.setConservativeForwardSelection(false);
+			search.setSearchBackwards(false);
+			search.setGenerateRanking(true);
+			
 			attrFilter.setEvaluator(attrEvaluator);
 			attrFilter.setSearch(search);
 			try {
@@ -64,16 +70,17 @@ System.out.println("Working Directory = " + System.getProperty("user.dir"));
 				Instances reduced_data_testing = Filter.useFilter(testData, attrFilter);
 
 				// should print out selected features
-				Enumeration<Attribute> attributes_training = reduced_data_training.enumerateAttributes();
-				while (attributes_training.hasMoreElements()) {
-					Attribute attr = attributes_training.nextElement();
-					System.out.println(reduced_data_training.attributeStats(attr.index()));
-				}
-				Enumeration<Attribute> attributes_testing = reduced_data_testing.enumerateAttributes();
-				while (attributes_testing.hasMoreElements()) {
-					Attribute attr = attributes_testing.nextElement();
-					System.out.println(reduced_data_testing.attributeStats(attr.index()));
-				}
+				System.out.println(reduced_data_training.toSummaryString());
+//				Enumeration<Attribute> attributes_training = reduced_data_training.enumerateAttributes();
+//				while (attributes_training.hasMoreElements()) {
+//					Attribute attr = attributes_training.nextElement();
+//					System.out.println(reduced_data_training.attributeStats(attr.index()));
+//				}
+//				Enumeration<Attribute> attributes_testing = reduced_data_testing.enumerateAttributes();
+//				while (attributes_testing.hasMoreElements()) {
+//					Attribute attr = attributes_testing.nextElement();
+//					System.out.println(reduced_data_testing.attributeStats(attr.index()));
+//				}
 
 				// apply classifier
 				NaiveBayes classifier = new NaiveBayes();
@@ -82,10 +89,14 @@ System.out.println("Working Directory = " + System.getProperty("user.dir"));
 				// should print out classifier data
 				System.out.println(classifier);
 
-				Evaluation evaluator = new Evaluation(reduced_data_testing);
-
-				// should print out evaluator data
-				System.out.println(evaluator.rootMeanPriorSquaredError());
+				Evaluation evaluator = new Evaluation(reduced_data_training);
+//				for (int i =0; i < reduced_data_training.size(); i++) {
+					evaluator.evaluateModel(classifier, reduced_data_testing);
+//					evaluator.evaluateModelOnce(classifier, reduced_data_testing.get(i));
+//				}
+					
+					// should print out evaluator data
+				System.out.println(evaluator.toSummaryString());
 			} catch (Exception exp) {
 		        System.err.println("Attribute selection filter failed. Reason: " + exp.getMessage());
 			}
